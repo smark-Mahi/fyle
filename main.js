322a409c1807c594164975906f1af871a0de3641
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-07-17 21:21:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".slider-wrapper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  // autoplay: {
+  //   delay: 2500,
+  //   reverseDirection: true,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
