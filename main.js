42946163193528aea1e0eb4af2a0309b3f7392a7
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-07-18 10:11:45 +0530

    chngs

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 var swiper = new Swiper(".slider-wrapper", {
   slidesPerView: 4,
   spaceBetween: 20,
-  // autoplay: {
-  //   delay: 2500,
-  //   reverseDirection: true,
-  // },
+  autoplay: {
+    delay: 2500,
+    reverseDirection: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
